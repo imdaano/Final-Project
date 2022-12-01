6c88929069eae23f19883f2436c0814a0151e5ea
mariosepulveda3 <mariosepul3@gmail.com>
2022-12-02 00:30:13 +0100

    FEAT: auth redux with functions and reducer added

--- src/redux/checkpoint/chekpointReducer.js
+++ src/redux/checkpoint/chekpointReducer.js
@@ -29,6 +29,50 @@ const checkpointReducer = (state = INITIAL_STATE, action) => {
         checkpoint: {},
         error: action.payload,
       };
+      case "searchCheckpoints":
+        return { ...state, search: action.payload, error: false };
+  
+      case "postingCheckpoints":
+        return { ...state, isLoading: true };
+  
+      case "postCheckpoints":
+        return {
+          ...state,
+          isLoading: false,
+          checkpoints: action.payload,
+          error: false,
+        };
+  
+      case "errorPostCheckpoints":
+        return { ...state, isLoading: false, checkpoints: [], error: action.payload };
+  
+      case "puttingCheckpoints":
+        return { ...state, isLoading: true };
+  
+      case "putCheckoints":
+        return {
+          ...state,
+          isLoading: false,
+          checkpoints: action.payload,
+          error: false,
+        };
+  
+      case "errorputCheckpoints":
+        return { ...state, isLoading: false, checkpoints: [], error: action.payload };
+  
+      case "deletingCheckpoints":
+        return { ...state, isLoading: true };
+  
+      case "deleteCheckpoints":
+        return {
+          ...state,
+          isLoading: false,
+          checkpoints: action.payload,
+          error: false,
+        };
+  
+      case "errorDeleteCheckpoints":
+        return { ...state, isLoading: false, checkpoints: [], error: action.payload };
 
 
     default:
