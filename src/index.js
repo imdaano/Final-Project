4a99799cf3faa9de6348991af1c34be9f3724fb9
mariosepulveda3 <mariosepul3@gmail.com>
2022-12-01 23:26:36 +0100

    FIX: fixed error od BrowserRouter as Router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/shared/services/api.js
+++ src/shared/services/api.js
@@ -13,7 +13,7 @@ const APIHeaders = {
 
 
 export const API = axios.create({
-    baseURL: process.env.TRAVELINGBOOKS_API_BACK_URL,
+    baseURL: "http://localhost:4000/",
     timeout: 5000,
     headers: APIHeaders
 });
@@ -32,7 +32,7 @@ const APIHeaders2 = {
 
 
 export const API2 = axios.create({
-    baseURL: process.env.TRAVELINGBOOKS_API_BACK_URL,
+    baseURL: "http://localhost:4000/",
     timeout: 5000,
     headers: APIHeaders2
 });
