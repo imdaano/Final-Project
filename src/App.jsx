a33c01ee147e2f089f09ed38a5293729f00c8ed9
JoseManuel88 <113127826+JoseManuel88@users.noreply.github.com>
2022-12-01 18:23:25 +0100

    Routes app Jose

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import "./App.scss";
 import MapView from "./components/MapView";
 import Checkpoints from "./pages/Checkpoints";
 import Home from "./pages/Home";
+import CheckpointDetails from "./pages/CheckpointDetails";
+import Favorites from "./pages/Favorites";
+import Login from "./pages/Login";
+import MyAccount from "./pages/MyAccount";
+import NewBook from "./pages/NewBook";
+import NewCheckpoint from "./pages/NewCheckpoint";
+import Register from "./pages/Register";
+import BookInfo from "./pages/BookInfo";
+import Books from "./pages/Books";
+
 
 function App() {
   return (
@@ -11,6 +21,15 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/map' element={<MapView />}/>
         <Route path='/checkpoints' element={<Checkpoints />}/>
+        <Route path='/checkpointDetails' element={<CheckpointDetails />}/>
+        <Route path='/favorites' element={<Favorites />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/myAccount' element={<MyAccount />}/>
+        <Route path='/newBook' element={<NewBook />}/>
+        <Route path='/myAccount' element={<NewCheckpoint />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/bookInfo' element={<BookInfo />}/>
+        <Route path='/books' element={<Books />}/>
       </Routes>
     </div>
   );
