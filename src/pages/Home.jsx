0c65a7c811deb429c37a74625569111e6978fce6
mariosepulveda3 <mariosepul3@gmail.com>
2022-12-02 10:20:16 +0100

    FEAT: home page added with button links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react-router-dom'
+import React from "react-router-dom";
+import ReusableButton from "../components/Button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <ReusableButton
+        clase={"button--books"}
+        text={<Link to="/books">Books</Link>}
+      />
+      <ReusableButton
+        clase={"button--checkpoints"}
+        text={<Link to="/checkpoints">Checkpoints</Link>}
+      />
+      <ReusableButton clase={"button--map"} text={<Link to="/map">Map</Link>} />
+      <ReusableButton
+        clase={"button--myaccount"}
+        text={<Link to="/myAccount">My Account</Link>}
+      />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
