c054a554aa37545915a586d46f485762499239c4
imdaano <iron-man@avengers.com>
2022-12-03 17:56:47 +0100

    FIX: corrections scss

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,16 +6,16 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <Link to="/coords" >
-        <img src="../assets/iconsNav/ubication.png" alt="map"/>
+        <img src="https://cdn-icons-png.flaticon.com/512/854/854980.png" className="map" alt="map"/>
       </Link>
       <Link to="/checkpoints" >
-        <img src="../assets/iconsNav/casita.png" alt="checkpoint"/>
+        <img src="https://img.icons8.com/ios7/12x/home.png" className="checkpoint" alt="checkpoint"/>
       </Link>
       <Link to="/books" >
-        <img src="../assets/iconsNav/book.png" alt="book"/>
+        <img src="https://cdn-icons-png.flaticon.com/512/130/130304.png" className="books" alt="book"/>
       </Link>
       <Link to="/myAccount" >
-        <img src="../assets/iconsNav/user.png" alt="user"/>
+        <img src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png" className="profile" alt="user"/>
       </Link>
     </div>
   );
